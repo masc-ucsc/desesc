b50921b7c02b2ebc1610f7620dacccf66bfe0cad
Jose Renau <renau@ucsc.edu>
2023-02-20 15:26:31 -0800

    name overload

--- mem/unmemxbar.cpp
+++ mem/unmemxbar.cpp
@@ -5,9 +5,9 @@
 #include "config.hpp"
 #include "memory_system.hpp"
 
-UnMemXBar::UnMemXBar(Memory_system *current, const std::string &section, const std::string &name)
+UnMemXBar::UnMemXBar(Memory_system *current, const std::string &section, const std::string &n)
     /* constructor {{{1 */
-    : GXBar(section, name) {
+    : GXBar(section, n) {
   Xbar_unXbar_balance--;  // decrement balance of XBars
   lower_level = NULL;
 

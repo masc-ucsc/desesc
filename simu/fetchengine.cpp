c3c0188687aea998c8658b17f6f546a49eacb995
Jose Renau <renau@ucsc.edu>
2025-05-01 10:21:56 -0700

    fix dinst scrap

--- simu/fetchengine.cpp
+++ simu/fetchengine.cpp
@@ -197,6 +197,18 @@ void FetchEngine::realfetch(IBucket *bucket, std::shared_ptr<Emul_base> eint, Ha
         }
       }
     }
+    if (dinst->getInst()->isControl() && dinst->isTaken()) {
+      avgBB.sample(fetch_width - (n2Fetch - last_taken), dinst->has_stats());
+      last_taken = n2Fetch;
+      maxBB--;
+      if (maxBB < 1) {
+        avgFB.sample(fetch_width - n2Fetch, dinst->has_stats());
+        avgBeyondFBInst.sample(n2Fetch, dinst->has_stats());
+        dinst->scrap();
+        break;
+      }
+      dinst->set_zero_delay_taken();
+    }
     lastpc = dinst->getPC();
     I(lastpc);
 
@@ -227,19 +239,6 @@ void FetchEngine::realfetch(IBucket *bucket, std::shared_ptr<Emul_base> eint, Ha
     // I(dinst->getGProc());
 
     if (dinst->getInst()->isControl()) {
-      if (dinst->isTaken()) {
-        avgBB.sample(fetch_width - (n2Fetch - last_taken), dinst->has_stats());
-        last_taken = n2Fetch;
-        maxBB--;
-        if (maxBB < 1) {
-          avgFB.sample(fetch_width - n2Fetch, dinst->has_stats());
-          avgBeyondFBInst.sample(n2Fetch, dinst->has_stats());
-          dinst->scrap();
-          break;
-        }
-        dinst->set_zero_delay_taken();
-      }
-
       bool stall_fetch = processBranch(dinst);
       if (stall_fetch) {
         avgFetchStallInst.sample(fetch_width - n2Fetch, dinst->has_stats());
